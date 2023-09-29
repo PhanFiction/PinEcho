0ea60ea03c64399a00a5f727738a709d4b0274b3
Steve <stevewu24@gmail.com>
2023-09-28 21:46:25 -0700

    added comment section

---
+++ frontend/src/components/Comment/Comment.jsx
@@ -0,0 +1,28 @@
+import './Comment.css';
+
+const Comment = ({ name, comment, likes, timeStamp, children }) => {
+  return(
+    <article className="comment-section">
+      <div className="profile-container">
+        {children}
+      </div>
+      <div className="comment-items">
+        <div className="comment">
+          <p>{name}</p>
+          <p>{comment}</p>
+        </div>
+        <ul>
+          <li>
+            {timeStamp}
+          </li>
+          <li>
+            <img src={require('../../assets/heart.png')} alt="heart" />
+            {likes}
+          </li>
+        </ul>
+      </div>
+    </article>
+  )
+};
+
+export default Comment;
