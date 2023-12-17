6f2d7538ca51e34c92d7b0060d8cae89139560d4
Steve <stevewu24@gmail.com>
2023-12-17 01:34:06 -0800

    updated the css to use tailwind

--- frontend/src/components/Comment/Comment.jsx
+++ frontend/src/components/Comment/Comment.jsx
@@ -1,23 +1,26 @@
-
+import React from 'react';
+import '../../styles/globals.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const Comment = ({ name, comment, likes, timeStamp, children }) => {
   return(
-    <article className="comment-section">
+    <article className="flex gap-4">
       <div>
         {children}
       </div>
-      <div className="comment-items">
-        <div className="comment">
-          <p>{name}</p>
+      <div className="flex flex-col">
+        <div className="flex">
+          <p className="font-semibold mr-2">{name}</p>
           <p>{comment}</p>
         </div>
-        <ul>
-          <li>
-            {timeStamp}
+        <ul className="flex items-center">
+          <li className="">
+            <span>{timeStamp}</span>
           </li>
-          <li>
-            <img src={require('../../assets/heart.png')} alt="heart" />
-            {likes}
+          <li className="flex items-center ml-8">
+            <FontAwesomeIcon className="mt-1 cursor-pointer hover:text-red" icon={faHeart}/>
+            <span className="ml-2 mt-0.5">{likes}</span>
           </li>
         </ul>
       </div>
