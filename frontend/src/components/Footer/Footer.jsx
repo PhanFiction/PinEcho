729fc721f67e64231eefaf983f47ae3eaef9c387
Steve <stevewu24@gmail.com>
2023-12-17 01:36:21 -0800

    replaced the css to use tailwind

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,8 +2,8 @@
 
 const Footer = () => {
   return(
-    <footer className="footer">
-      <ul>
+    <footer className="w-full bg-darkslategray-400">
+      <ul className="text-center text-white p-4">
         <li>Created By Steve Wu</li>
       </ul>
     </footer>
