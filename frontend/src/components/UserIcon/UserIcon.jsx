0fdf3377196c4978eb328ac24e83585f12261021
Steve <stevewu24@gmail.com>
2023-12-19 23:21:22 -0800

    updated UserIcon to show only icon of user

--- frontend/src/components/UserIcon/UserIcon.jsx
+++ frontend/src/components/UserIcon/UserIcon.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import '../../styles/globals.css';
 
-const UserIcon = ({ name, children }) => {
-  const firstLetter = name.split('')[0];
+const UserIcon = ({ name, iconSize=18, children }) => {
+  const firstLetter = name.slice(0, 1); // Use slice to get the first letter
+
   return (
-    <div className="flex flex-col items-center justify-center h-8 w-8 rounded-full ring-2 ring-black">
-      <span className="text-center font-semibold text-[18px]"> {firstLetter} {children} </span>
-    </div>
-  )
+    <span className={`text-center font-semibold text-${iconSize}`}> {firstLetter} {children} </span>
+  );
 };
 
 export default UserIcon;
