69c4a0d7d85a09b34e3e7f5f0fb68f85cb1693c5
Steve <stevewu24@gmail.com>
2023-12-20 23:18:58 -0800

    updated footer css to be placed at the bottom of the page

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return(
-    <footer className="w-full bg-darkslategray-400">
+    <footer className="w-full bg-darkslategray-400 absolute bottom-0 md:bottom-auto lg:bottom-0">
       <ul className="text-center text-white p-4">
         <li>Created By Steve Wu</li>
       </ul>
