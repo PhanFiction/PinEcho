cef8ed7906d9f3e77d4af52c602b6460359dfbb1
Steve <stevewu24@gmail.com>
2023-12-22 23:18:12 -0800

    updated layout to have smooth scroll and show scroll for height

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,12 +1,13 @@
 import Nav from "./Nav/Nav"
-import Footer from "./Footer/Footer"
+import '../styles/globals.css';
  
 export default function Layout({ children }) {
   return (
     <>
       <Nav />
+      <main id="main" className="scroll-smooth overflow-y-scroll h-screen mt-12">
         {children}
-      <Footer />
+      </main>
     </>
   )
 }
