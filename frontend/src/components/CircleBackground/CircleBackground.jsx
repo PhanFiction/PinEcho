9cad935bf3bbc111286a04e49d7b41c12345dbdf
Steve <stevewu24@gmail.com>
2023-12-24 23:22:01 -0800

    updated circlebackground component to use sm, md, lg size

--- frontend/src/components/CircleBackground/CircleBackground.jsx
+++ frontend/src/components/CircleBackground/CircleBackground.jsx
@@ -2,14 +2,19 @@
 import React from "react";
 import '../../styles/globals.css';
 
-const CircleBackground = ({ sm=true, children }) => {
+const CircleBackground = ({ sm = true, md = false, lg = false, children }) => {
+  let sizeClass;
+
+  if (sm) {
+    sizeClass = "w-6 h-6";
+  } else if (md) {
+    sizeClass = "w-12 h-12";
+  } else if (lg) {
+    sizeClass = "w-20 h-20";
+  }
+
   return (
-    sm ? 
-    <div className={`flex flex-col items-center justify-center w-6 h-6 rounded-full ring-1 ring-black`}>
-      {children}
-    </div>
-    :
-    <div className={`flex flex-col items-center justify-center w-12 h-12 rounded-full ring-1 ring-black`}>
+    <div className={`flex flex-col items-center justify-center rounded-full ring-1 ring-black ${sizeClass} relative`}>
       {children}
     </div>
   );
