1e5a523096cebdddbc3d622678a9571ee5fbbb03
Steve <stevewu24@gmail.com>
2023-12-27 22:41:17 -0800

    removed overflow of y direction on main

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -5,7 +5,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Nav />
-      <main id="main" className="scroll-smooth overflow-y-scroll h-screen mt-12">
+      <main id="main" className="scroll-smooth mt-12">
         {children}
       </main>
     </>
