9f4ed9b4ae49cae8e17df7aad179f6bb2ea55061
Steve <stevewu24@gmail.com>
2024-01-04 23:07:37 -0800

    updated Nav to display user profile picture when logged in

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,38 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import '../../styles/globals.css';
 import Icon from '../Icon/Icon';
 import Link from 'next/link';
-import '../../styles/globals.css';
-
-import UserIcon from '../../assets/images/user-circle.svg';
+import { useRouter } from 'next/navigation';
 import PinEcho from '../../assets/images/PinEcho.svg';
 import Dropdown from '../Dropdown/Dropdown';
+import {fetchCredentials, fetchUserCredentials, logout } from '../../utils/auth';
+import CircleBackground from '../CircleBackground/CircleBackground';
+import UserIcon from '../UserIcon/UserIcon';
 
 const Nav = () => {
+  const router = useRouter();
   const [isOpen, setIsOpen] = useState(false);
+  const [authenticated, setAuthenticated] = useState(false);
+  const [user, setUser] = useState(fetchUserCredentials());
 
   const toggleDropdown = () => {
     setIsOpen((prevIsOpen) => !prevIsOpen);
   };
 
+  const handleLogout = () => {
+    const loggedOut = logout();
+    if(loggedOut) {
+      setAuthenticated(false);
+      router.push('/login');
+    }
+  }
+
+  useEffect(() => {
+    // Check the user's authentication status using the utility function
+    const isAuthenticated = fetchCredentials();
+
+    // Update the state based on the authentication status
+    if(isAuthenticated) {
+      setAuthenticated(true);
+    }else{
+      setAuthenticated(false);
+    }
+  }, []);
+
   return(
-    <nav className="w-full flex justify-between p-4 bg-snow drop-shadow-lg">
+    <nav className="fixed left-0 right-0 top-0 flex justify-between p-4 bg-snow drop-shadow-lg z-40">
       <Link href="/" className="flex justify-center align-center">
         <Icon className="m-0 p-0" iconName={PinEcho} altName={'pin'}/>
       </Link>
-      <ul className="flex items-center gap-4 text-white">
-        <li className="hover:bg-brightred bg-darkred py-1 px-4 rounded-lg ease-in duration-200">
-          <Link href="/landing"><span className="text-sm text-black hover:text-indianred-200">Login</span></Link>
-        </li>
-        <li className="hover:bg-brightred bg-darkred py-1 px-4 rounded-lg ease-in duration-200">
-          <Link href="/signup"><span className="text-sm text-black hover:text-indianred-200">Sign Up</span></Link>
-        </li>
-        <li>
-          <span className="cursor-pointer" onClick={toggleDropdown}>
-            <Icon iconName={UserIcon} altName={'userIcon'} width={32} height={32} />
-          </span>
-          <Dropdown isOpen={isOpen} setIsOpen={toggleDropdown}/>
-        </li>
-      </ul>
+      {
+        authenticated ? (
+          <ul className="flex items-center gap-4 text-white">
+            <li className="hover:bg-brightred bg-darkred py-1 px-4 rounded-lg ease-in duration-200">
+              <Link href="/pin/create"><span className="text-sm text-black hover:text-indianred-200">Create Pin</span></Link>
+            </li>
+            <li>
+              <span className="hover:cursor-pointer text-black" onClick={toggleDropdown}>
+                <CircleBackground md={true}>
+                  <UserIcon name={user.username} imgName={user.profileImage.path ? user.profileImage.path : ""}/>
+                </CircleBackground>
+              </span>
+              <Dropdown isOpen={isOpen} setIsOpen={toggleDropdown} handleLogout={handleLogout}/>
+            </li>
+          </ul>
+        )
+        : 
+        (
+          <ul className="flex items-center gap-4 text-white">
+            <li className="hover:bg-brightred bg-darkred py-1 px-4 rounded-lg ease-in duration-200">
+              <Link href="/login"><span className="text-sm text-black hover:text-indianred-200">Login</span></Link>
+            </li>
+            <li className="hover:bg-brightred bg-darkred py-1 px-4 rounded-lg ease-in duration-200">
+              <Link href="/signup"><span className="text-sm text-black hover:text-indianred-200">Sign Up</span></Link>
+            </li>
+          </ul>
+        )
+      }
     </nav>
   )
 };
