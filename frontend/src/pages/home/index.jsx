60e03f908a7a4368e38b83af7dfc84146635ac88
Steve <stevewu24@gmail.com>
2024-01-08 09:21:18 -0800

    removed fakeData images and updated the jsx to display images if the data fetched from the server has any data in it

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,99 +1,32 @@
 "use client"
 import React, { useState, useEffect } from 'react';
-import Layout from '../../components/Layout';
-import LazyImage from '../../components/LazyImage/LazyImage';
 import '../../styles/globals.css';
-
-const initialImages = [
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-]
-
-const additionalImages = [
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1685007029/pins/meenva3kqyxe6cb9shgl.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1685005181/pins/m7y9ivov80vy6cskztqq.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1684964447/pins/fhl3cwe4hg8zkl8g6tpc.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1685004760/pins/c8g9ihvxhqpukpubgn97.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1684964447/pins/fhl3cwe4hg8zkl8g6tpc.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1685005071/pins/j4nvuoc21ojasiqjspd0.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1684964447/pins/fhl3cwe4hg8zkl8g6tpc.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1685004760/pins/c8g9ihvxhqpukpubgn97.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1684964447/pins/fhl3cwe4hg8zkl8g6tpc.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1685005071/pins/j4nvuoc21ojasiqjspd0.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1685007029/pins/meenva3kqyxe6cb9shgl.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1685005181/pins/m7y9ivov80vy6cskztqq.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1684964447/pins/fhl3cwe4hg8zkl8g6tpc.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1685004760/pins/c8g9ihvxhqpukpubgn97.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1684964447/pins/fhl3cwe4hg8zkl8g6tpc.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1685005071/pins/j4nvuoc21ojasiqjspd0.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1684964447/pins/fhl3cwe4hg8zkl8g6tpc.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1685004760/pins/c8g9ihvxhqpukpubgn97.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1684964447/pins/fhl3cwe4hg8zkl8g6tpc.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1685005071/pins/j4nvuoc21ojasiqjspd0.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1688024735/pins/lnz9ysehubweak08hu8b.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1685007029/pins/meenva3kqyxe6cb9shgl.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1685007029/pins/meenva3kqyxe6cb9shgl.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1685005181/pins/m7y9ivov80vy6cskztqq.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1684964447/pins/fhl3cwe4hg8zkl8g6tpc.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1685004760/pins/c8g9ihvxhqpukpubgn97.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1684964447/pins/fhl3cwe4hg8zkl8g6tpc.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1685005071/pins/j4nvuoc21ojasiqjspd0.webp',
-  'https://res.cloudinary.com/dvxeoc3ny/image/upload/v1684964447/pins/fhl3cwe4hg8zkl8g6tpc.webp',
-]
+import Layout from '../../components/Layout';
+import ImageGrid from '../../components/ImageGrid/ImageGrid';
+import { getAllPins } from '../../service/pinService';
 
 const Home = () => {
-  const [loadedImages, setLoadedImages] = useState(initialImages);
-  const [pageNumber, setPageNumber] = useState(1);
+  const [data, setData] = useState('');
 
   useEffect(() => {
-    const handleScroll = () => {
-      const isNearBottom = window.innerHeight + window.scrollY >= document.documentElement.offsetHeight - 100;
-
-      if (isNearBottom) {
-        // User is near the bottom of the page, load more content
-        setLoadedImages((prevImages) => [...prevImages, ...additionalImages]);
-        setPageNumber((prevPage) => prevPage + 1);
-      }
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
+    const fetchPosts = async () => {
+      const req = await getAllPins();
+      setData(req.data);
+    }
+    fetchPosts();
   }, []);
 
-  return(
+  return (
     <Layout>
-      <section className="p-4 sm:p-8">
-        <div className="columns-1 gap-4 sm:columns-2 sm:gap-4 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8">
-          {
-            loadedImages.map((item, index) => (
-              <LazyImage
-                key={index}
-                src={item}
-                alt={`item ${index}`}
-                width={500}
-                height={500}
-              />
-            ))
-          }
-          </div>
-      </section>
+      {
+        data?.length < 1 ? (
+          <h1>Empty</h1>
+        )
+        : 
+        (
+          <ImageGrid initialImages={data} additionalImages={data}/>
+        )
+      }
     </Layout>
   )
 };
