2b50431f25e7b7ca8ca349c69a17c920098d58fe
Steve <stevewu24@gmail.com>
2024-01-08 08:58:50 -0800

    update getAllPins with try and catch, fetchsinglepin to return certain data, refactored code to upload image to cloudinary in different file, created returning saves for users, created function to update Saves if they exist in db, created update comment like

--- backend/controller/pinController.js
+++ backend/controller/pinController.js
@@ -1,63 +1,82 @@
 const Pin = require('../models/Pin');
 const User = require('../models/User');
 const Comment = require('../models/Comment');
-const config = require('../config/config');
 const verifyToken = require('../utils/verifyToken');
 const convertIdToString = require('../utils/idToString');
+const { deleteImg, uploadImg } = require('../utils/cloudinaryService');
 
 // returns all pins
 exports.getAllPins = async (req, res) => {
-  const pins = await Pin.find({});
-  res.status(200).send({data: pins});
+  try {
+    const pins = await Pin.find({}).populate({path: 'comments creator', select: 'username profileImage'});
+    res.status(200).send({ data: pins});
+  } catch(error) {
+    // Send a 500 Internal Server Error response with a generic error message
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
 };
 
 // return a single pin
 exports.getSinglePin = async (req, res) => {
-  const pinId = req.params.id;
-  const foundPin = await Pin.findById(pinId);
-  res.status(200).send(foundPin);
+  try {
+    const pinId = req.params.id;
+    const foundPin = await Pin.findById(pinId)
+      .populate({path: 'comments', populate: { path: 'creator', select: 'username creator profileImage' }})
+      .populate({path: 'creator', select: 'profileImage username'});
+
+    if (!foundPin) {
+      // If the pin is not found, send a 404 Not Found response
+      return res.status(404).send({ error: 'Pin not found' });
+    }
+
+    res.status(200).send(foundPin);
+  } catch (error) {
+    // Send a 500 Internal Server Error response with a generic error message
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
 };
 
 // store to cloudinary and return path
 exports.createPin = async (req, res) => {
-  const { imgURL, title, description, altText, link, category } = req.body;
-  const cookie = req.headers.cookie.split(';')[0].split("authToken=")[1]; // split and return cookie
-  const decodedToken = verifyToken(cookie);
+  try {
+    const { imgURL, title, description, altText, link, category } = req.body;
+    const cookie = req.headers.cookie.split(';')[0].split("authToken=")[1]; // split and return cookie
+    const decodedToken = verifyToken(cookie);
 
-  const foundUser = await User.findById(decodedToken.id);
+    if(!decodedToken) return res.status(401).send({error: 'Not authorized'});
 
-  if(imgURL === "") return res.status(400).send({error: "missing image"});
-  if(!foundUser) return res.status(401).send({error: 'User not found'});
-  const transformation = {
-    width: 500, 
-    height: 400,
-    crop: "scale",
-  };
-
-  // store to cloudinary
-  const result = await config.cloudinaryService.uploader.upload(imgURL,transformation);
-
-  // create new pin
-  const newPin = new Pin({
-    title,
-    description,
-    creator: foundUser._id,
-    altText,
-    link,
-    category,
-    imgPath: {
-      path: result.url,
-      publicId: result.public_id,
-    },
-    comments: [],
-    likes: 0,
-  });
-
-    const savedPin = await newPin.save();
-    const pinStringId = convertIdToString(savedPin._id);
-    foundUser.posts.push(pinStringId);
-    await foundUser.save();
-    res.status(201).send({success: 'pin created', pinId: pinStringId});
+    const foundUser = await User.findById(decodedToken.id);
+  
+    if(imgURL === "") return res.status(400).send({error: "missing image"});
+    if(!foundUser) return res.status(401).send({error: 'User not found'});
+  
+    // store to cloudinary
+    const result = await uploadImg(imgURL, 'posts');;
+  
+    // create new pin
+    const newPin = new Pin({
+      title,
+      description,
+      creator: foundUser._id,
+      altText,
+      link,
+      category,
+      imgPath: {
+        path: result.url,
+        publicId: result.public_id,
+      },
+      comments: [],
+      likes: [],
+    });
+  
+      const savedPin = await newPin.save();
+      const pinStringId = convertIdToString(savedPin._id);
+      foundUser.posts.push(pinStringId);
+      await foundUser.save();
+      res.status(201).send({success: 'pin created', pinId: pinStringId});
+  } catch (error) {
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
 };
 
 // update the pin
@@ -103,7 +122,7 @@ exports.deletePin = async (req, res) => {
   if(foundPin.creator != decodedToken.id) return res.status(401).send({error: 'Not authorized'});
 
   try{
-    await config.cloudinaryService.uploader.destroy(foundPin.imgPath.publicId);
+    await deleteImg(foundPin.imgPath.publicId, 'posts');
     await Pin.findByIdAndDelete(pinId);
 
     foundUser.posts.map(i => i.id !== pinId);
@@ -114,30 +133,84 @@ exports.deletePin = async (req, res) => {
   }
 };
 
-exports.updateLike = async (req, res) => {
-  const pinId = req.params.id;
-  const cookie = req.headers.cookie.split(';')[0].split("authToken=")[1]; // split and return cookie
-  const decodedToken = verifyToken(cookie);
-
-  if(!decodedToken ) return res.status(401).send({error: 'Not authorized'});
-  if(!foundPin) return res.status(401).send({error: 'Pin is not available'});  
-  if(!foundUser) return res.status(401).send({error: 'User not found'});
-
-  const foundPin = await Pin.findById(pinId);
-  const foundUser = await User.findById(decodedToken.id);
-  
+exports.getSaves = async (req, res) => {
   try {
+    const cookie = req.headers.cookie.split(';')[0].split("authToken=")[1];
+    const decodedToken = verifyToken(cookie);
+    const foundUser = await User.findById(decodedToken.id).select('saves')
+      .populate({path: 'saves', populate: { path: 'creator', select: 'username creator profileImage' }});
+
+    if(!decodedToken) return res.status(401).send({error: 'Not authorized'});
+    if(decodedToken.id !== foundUser.id) return res.status(401).send({error: 'Not authorized'});
+    if(!foundUser) return res.status(401).send({error: 'User does not exist'});
+    res.status(201).send(foundUser);
+  } catch(error) {
+    res.status(401).send({error});
+  }
+}
+
+exports.updatePinSaves = async (req, res) => {
+  try {
+    const pinId = req.params.id;
+    const cookie = req.headers.cookie.split(';')[0].split("authToken=")[1]; // split and return cookie
+    const decodedToken = verifyToken(cookie);
+
+    if (!decodedToken) {
+      return res.status(401).send({ error: 'Not authorized' });
+    }
+
+    const foundUser = await User.findById(decodedToken.id);
+
+    if (!foundUser) {
+      return res.status(401).send({ error: 'User does not exist' });
+    }
+
+    // Check if the pinId is already in the saves array
+    const isPinAlreadySaved = foundUser.saves.includes(pinId);
+
+    if (isPinAlreadySaved) {
+      // If the pin is already saved, remove it from saves
+      foundUser.saves = foundUser.saves.filter(savedPinId => savedPinId.toString() !== pinId.toString());
+    } else {
+      // If the pin is not saved, add it to saves
+      foundUser.saves.push(pinId);
+    }
+
+    // Save the updated user
+    const savedUser = await foundUser.save();
+
+    // Return the updated user object
+    res.status(201).send(savedUser);
+  } catch (error) {
+    console.error('Error updating saves:', error);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
+};
+
+// Todo: Test code and see if the likes is getting updated
+exports.updatePinLike = async (req, res) => {
+  try {
+    const pinId = req.params.id;
+    const cookie = req.headers.cookie.split(';')[0].split("authToken=")[1];
+    const decodedToken = verifyToken(cookie);
+
+    const foundPin = await Pin.findById(pinId);
+    const foundUser = await User.findById(decodedToken.id);
+
+    if(!decodedToken) return res.status(401).send({error: 'Not authorized'});
+    if(!foundPin) return res.status(401).send({error: 'Pin is not available'});
+    if(!foundUser) return res.status(401).send({error: 'User not found'});
+
     if(foundPin.likes.includes(decodedToken.id) === true) {
-      foundPin.likes = foundPin.likes - 1;
-      foundUser.likes = foundUser.likes.map(item => item.id !== decodedToken.id);
+      foundUser.pinLikes.map(id => id.toString() !== pinId.toString());
+      foundPin.likes.map(id => id.toString() !== decodedToken.id.toString());
     }else{
-      foundPin.likes = foundPin.likes + 1;
-      foundUser.likes = foundUser.likes.push(pinId);
+      foundPin.likes.push(decodedToken.id);
+      foundUser.pinLikes.push(pinId);
     }
     await foundPin.save();
     await foundUser.save();
-    const pinLikes = foundPin.likes.length;
-    res.status(201).send({'successfully updated likes': pinLikes})
+    res.status(201).send({'successfully updated likes': foundPin.pinLikes})
   }catch(error){
     res.status(401).send({error});
   }
@@ -145,16 +218,16 @@ exports.updateLike = async (req, res) => {
 
 exports.createComment = async (req, res) => {
   const pinId = req.params.id;
-  const cookie = req.headers.cookie.split(';')[0].split("authToken=")[1]; // split and return cookie
+  const cookie = req.headers.cookie.split(';')[0].split("authToken=")[1];
   const decodedToken = verifyToken(cookie);
-  const comment = req.body;
+  const { comment } = req.body;
+
+  const foundPin = await Pin.findById(pinId);
+  const foundUser = await User.findById(decodedToken.id);
 
   if(!decodedToken) return res.status(401).send({error: 'Not authorized'});
   if(!foundPin) return res.status(401).send({error: 'Pin is not available'}); 
   if(!foundUser) return res.status(401).send({error: 'User not found'});
-
-  const foundPin = await Pin.findById(pinId);
-  const foundUser = await User.findById(decodedToken.id);
 
   const newComment = {
     creator: decodedToken.id,
@@ -172,18 +245,18 @@ exports.createComment = async (req, res) => {
     foundPin.comments.push(commetId);
     foundUser.comments.push(commetId);
 
+    console.log(foundPin);
     // save the newly added id to database
-    await foundUser.save();
-    await foundPin.save();
+    //await foundUser.save();
+    //await foundPin.save();
+    //res.status(201).send({'successfully created comment': foundPin.comments})
   }catch(error){
     res.status(401).send({error});
   }
 };
 
-// pin/:id/comment/:id
 exports.updateComment = async (req, res) => {
-  const commentId = req.params.id;
-  const comment = req.body;
+  const { comment, commentId } = req.body;
   const foundComment = await Comment.findById(commentId);
 
   const cookie = req.headers.cookie.split(';')[0].split("authToken=")[1]; // split and return cookie
@@ -201,9 +274,39 @@ exports.updateComment = async (req, res) => {
   }
 };
 
+// Todo: Test code and see if the likes is getting updated
+exports.updateCommentLike = async (req, res) => {  
+  try {
+    const commentId = req.params.id;
+    const cookie = req.headers.cookie.split(';')[0].split("authToken=")[1]; // split and return cookie
+    const decodedToken = verifyToken(cookie);
+
+    if(!decodedToken ) return res.status(401).send({error: 'Not authorized'});
+
+    const foundComment = await Comment.findById(commentId);
+    const foundUser = await User.findById(decodedToken.id);
+
+    if(!foundComment) return res.status(401).send({error: 'Comment is not available'});  
+    if(!foundUser) return res.status(401).send({error: 'User not found'});
+
+    if (foundComment.likes.includes(decodedToken.id)) {
+      // If the user already liked the comment, remove the like
+      foundComment.likes = foundComment.likes.filter(userId => userId.toString() !== foundUser._id.toString());
+    } else {
+      // If the user hasn't liked the comment, add the like
+      foundComment.likes.push(foundUser._id);
+    }    
+    await foundComment.save();
+    const commentLikes = foundComment.likes.length;
+    res.status(201).send({'successfully updated likes': commentLikes})
+  }catch(error){
+    res.status(401).send({error});
+  }
+};
+
 exports.deleteComment = async (req, res) => {
   const commentId = req.params.id;
-  const cookie = req.headers.cookie.split(';')[0].split("authToken=")[1]; // split and return cookie
+  const cookie = req.headers.cookie.split(';')[0].split("authToken=")[1];
   const decodedToken = verifyToken(cookie);
   const foundComment = await Comment.findById(commentId);
   const foundPin = await Pin.findById(foundComment.pin.id);
