cb5cf911986331fcaeb7e875f5d8af1e1a449fba
Steve <stevewu24@gmail.com>
2024-01-08 09:14:30 -0800

    created overlay component

---
+++ frontend/src/components/Overlay/Overlay.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../styles/globals.css';
+
+const Overlay = ({ children }) => {
+  return (
+    <div 
+      className="hidden overlay absolute inset-0
+      group-hover:bg-black/60 duration-300 rounded-md 
+      hover:cursor-pointer group-hover:flex justify-center items-center">
+      { children }
+    </div>
+  )
+}
+
+export default Overlay;
