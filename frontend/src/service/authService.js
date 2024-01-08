979d203ae86f0bd3b6c4b21d01cf3bee65bbc5a0
Steve <stevewu24@gmail.com>
2024-01-08 09:28:03 -0800

    created auth and pin service that uses axios to do http methods

---
+++ frontend/src/service/authService.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { config } from '../utils/auth';
+const api = process.env.NEXT_PUBLIC_API_URL;
+
+export const signUpService = async (credentials) => {
+  const res = await axios.post(`${api}/signup`, credentials, config);
+  return res.data;
+}
+
+export const loginService = async (credentials) => {
+  const res = await axios.post(`${api}/login`, credentials, config);
+  return res.data;
+}
+
+export const getUser = async (credentials) => {
+  const res = await axios.get(`${api}/user`, config);
+  return res.data;
+}
+
+export const logout = async () => {
+  const res = await axios.get(`${api}/logout`, config);
+  return res.data;
+}
---
+++ frontend/src/service/pinService.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import { config } from '../utils/auth';
+const api = process.env.NEXT_PUBLIC_API_URL;
+
+export const getAllPins = async () => {
+  const req = await axios.get(`${api}/pin/pins`);
+  return req.data;
+}
+
+export const getSinglePin = async (pinId) => {
+  const req = await axios.get(`${api}/pin/${pinId}`);
+  return req.data;
+}
+
+export const getSavedPins = async () => {
+  const req = await axios.get(`${api}/pin/saves`, config);
+  return req.data;
+}
+
+export const savePin = async (pinId) => {
+  const req = await axios.put(`${api}/pin/save/${pinId}`, {}, config);
+  return req.data;
+}
+
+export const getSaves = async () => {
+  const req = await axios.get(`${api}/pin/saves`);
+  return req.data;
+}
+
+export const updatePinLike = async (pinId) => {
+  const req = await axios.put(`${api}/pin/likes/${pinId}`, {}, config);
+  return req.data;
+}
+
+export const updateCommentLike = async (commentId) => {
+  const req = await axios.put(`${api}/pin/comment/likes/${commentId}`, {}, config);
+  return req.data;
+}
+
+export const createComment = async (pinId, data) => {
+  const req = await axios.post(`${api}/pin/comment/${pinId}`, data, config);
+  console.log(req.data);
+  return req.data;
+}
