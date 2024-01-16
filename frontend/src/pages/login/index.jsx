cf836756c3444b5178573d5d74e73c740d222a12
Steve <stevewu24@gmail.com>
2024-01-16 11:56:01 -0800

    removed import global styles, no longer needed

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,5 +1,4 @@
 import React,{ useState, useEffect } from "react";
-import '../../styles/globals.css';
 import Link from 'next/link';
 import { useRouter } from "next/router";
 import Cookies from 'js-cookie';
