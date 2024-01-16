509605355791ab1fce7016c7f5d2077806985c2a
Steve <stevewu24@gmail.com>
2024-01-16 11:50:38 -0800

    removed import global styles, no longer needed, added disable prop

--- frontend/src/components/ActionButton/ActionButton.jsx
+++ frontend/src/components/ActionButton/ActionButton.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import '../../styles/globals.css';
-
-const ActionButton = ({ handleClick, bgColor="red", children }) => {
-
+const ActionButton = ({ handleClick, bgColor="red", disable=false, children }) => {
   return (
     <button
       type="button" 
       className={`w-full bg-${bgColor} text-white p-1 rounded-md shadow-lg hover:bg-firebrick-200 ease-in duration-300`}
       onClick={handleClick}
+      disabled={disable}
     >
       { children }
     </button>
