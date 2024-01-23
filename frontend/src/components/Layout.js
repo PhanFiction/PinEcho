cf9214f3d69e08e039de8ad44957770418bef85d
Steve <stevewu24@gmail.com>
2024-01-23 11:26:10 -0800

    removed id in main

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Nav />
-      <main id="main" className="scroll-smooth mt-12">
+      <main className="scroll-smooth mt-12">
         {children}
       </main>
     </>
