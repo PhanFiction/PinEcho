93e00a1436fa4b58d097877448b9e6f19515fc49
Steve <stevewu24@gmail.com>
2024-01-23 11:30:27 -0800

    refactored Modal component code

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -1,39 +1,16 @@
-import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faComment, faXmark, faHeart } from '@fortawesome/free-solid-svg-icons';
-
-export default function Modal({ title, handlePinLike, pinLiked, children }) {
-  const [showModal, setShowModal] = useState(false);
+export default function Modal({ showModal, children }) {
   return (
     <>
-      <FontAwesomeIcon className="text-2xl hover:cursor-pointer" icon={faComment} onClick={() => setShowModal(true)}/>
       {showModal ? (
         <>
           <div
-            className="justify-center items-center flex overflow-x-hidden 
-            overflow-y-auto fixed bottom-0 left-0 right-0 z-50 outline-none focus:outline-none"
+            className="justify-center items-center flex overflow-x-hidden w-full
+            overflow-y-auto fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 outline-none focus:outline-none"
           >
             <div className="relative my-0 mx-auto">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                  <div className="w-full flex gap-4 items-center">
-                    <FontAwesomeIcon icon={faXmark} className="text-2xl" onClick={() => setShowModal(false)}/>
-                    <h4 className="text-lg font-semibold">
-                      {title} Comments
-                    </h4>
-                    <FontAwesomeIcon
-                      className={`cursor-pointer ease-in duration-300 ml-auto text-2xl ${pinLiked ? 'text-red': 'text-black'}`}
-                      icon={faHeart}
-                      onClick={handlePinLike}
-                    />
-                  </div>
-                </div>
-                {/*Body and Commenting button */}
-                <div className="mt-4">
-                  { children }
-                </div>
+                { children }
               </div>
             </div>
           </div>
