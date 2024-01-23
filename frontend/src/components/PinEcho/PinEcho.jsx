7c682b63bd8a2a6c0985231640bba2e4e168ce05
Steve <stevewu24@gmail.com>
2024-01-23 11:31:55 -0800

    updated pinecho component

--- frontend/src/components/PinEcho/PinEcho.jsx
+++ frontend/src/components/PinEcho/PinEcho.jsx
@@ -4,10 +4,10 @@ import CircleBackground from '../CircleBackground/CircleBackground';
 import ToggleArrow from '../ToggleArrow/ToggleArrow';
 import Comment from '../Comment/Comment';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretRight, faHeart } from '@fortawesome/free-solid-svg-icons';
-import Layout from '../Layout';
+import { faCaretRight, faHeart, faTrash, faComment } from '@fortawesome/free-solid-svg-icons';
 import Modal from '../Modal/Modal';
 import ModalComment from '../Modal/ModalComment';
+import ModalHeader from '../Modal/ModalHeader';
 import Image from 'next/image';
 import ActionButton from '../ActionButton/ActionButton';
 
@@ -15,15 +15,35 @@ const PinEcho = ({
     pinId, pinCreator, title, altText, description, 
     imgPath, comments, link, handleSavePin, 
     message, handleMessageChange, handleSubmitComment, 
-    user, handleCommentLike, pinSaved, pinLiked, handlePinLike, }) => {
-  const [show, showComment] = useState(false);
+    user, handleCommentLike, pinSaved, pinLiked, handlePinLike, handleDeletePin, isCreator }) => {
+  const [show, setShowComment] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
 
-  const toggleComments = () => showComment(!show);
+  const toggleComments = () => setShowComment(!show);
+  const toggleDeleteModal = () => setShowDeleteModal(!showDeleteModal);
 
   if(!(pinId || title || description || comments)) return (<></>);
 
   return (
-    <Layout>
+    <>
+      <Modal showModal={showDeleteModal}>
+        <div className="flex justify-center p-5 text-2xl">
+          <h3 className="font-semibold">Delete this Pin</h3>
+        </div>
+        <div className="p-5 text-md">
+          <p>Are you sure you want to delete the pin</p>
+        </div>
+        <div className="flex flex-col p-5 gap-4">
+          <ActionButton bgColor="lightgray" hoverColor="darkgray" textColor="text-black" handleClick={toggleDeleteModal}>
+            Cancel
+          </ActionButton>
+          <ActionButton handleClick={handleDeletePin}>
+            Confirm
+          </ActionButton>
+        </div>
+      </Modal>
+
       <section className="flex items-center justify-center h-full p-4 mt-24 lg:mt-8">
         <div className="w-11/12 lg:w-8/12 flex flex-col rounded-md gap-4 m-0 sm:m-2 lg:m-12 lg:flex-row shadow-md">
           <div className="w-full lg:w-8/12 overflow-hidden relative">
@@ -43,7 +63,7 @@ const PinEcho = ({
               <div className="w-28">
                 {
                   pinSaved ?
-                  <ActionButton bgColor="darkgray" handleClick={handleSavePin}>
+                  <ActionButton bgColor="firebrick" handleClick={handleSavePin}>
                     <span className="font-semibold text-white">
                       Saved
                     </span>
@@ -56,13 +76,25 @@ const PinEcho = ({
                   </ActionButton>
                 }
               </div>
-              <FontAwesomeIcon
-                className={`cursor-pointer ease-in duration-300 ml-auto text-2xl ${pinLiked ? 'text-red': 'text-black'}`}
-                icon={faHeart}
-                onClick={handlePinLike}
-              />
+              <div className="flex gap-4">
+                {
+                  isCreator ? 
+                  (
+                    <FontAwesomeIcon
+                      className={`cursor-pointer ease-in duration-300 ml-auto text-2xl hover:text-red text-black`}
+                      icon={faTrash}
+                      onClick={toggleDeleteModal}
+                    />
+                  ): <></>
+                }
+                <FontAwesomeIcon
+                  className={`cursor-pointer ease-in duration-300 ml-auto text-2xl hover:text-red text-black ${pinLiked ? 'text-red': 'text-black'}`}
+                  icon={faHeart}
+                  onClick={handlePinLike}
+                />
+              </div>
             </div>
-            <a href="/" className="text-red text-lg underline truncate">{link}</a>
+            <a href={link} target="href" className="text-red text-lg underline truncate">{link}</a>
             <h2 className="font-semibold text-xl">{title}</h2>
             <p className="font-opensans">{description}</p>
             <p className="font-opensans">{altText}</p>
@@ -80,7 +112,7 @@ const PinEcho = ({
               <div className="w-28 rounded-lg bg-red text-white">
                 {
                   pinSaved ?
-                  <ActionButton bgColor="darkgray" handleClick={handleSavePin}>
+                  <ActionButton bgColor="firebrick" handleClick={handleSavePin}>
                     <span className="font-semibold text-white">
                       Saved
                     </span>
@@ -93,42 +125,22 @@ const PinEcho = ({
                   </ActionButton>
                 }
               </div>
-              <Modal 
-                title={comments.length} 
-                handlePinLike={handlePinLike} 
-                pinLiked={pinLiked}
-              >
-                <div className="flex flex-col gap-4 w-full h-96">
-                {
-                  comments.length < 1 ? (
-                    <div className="mt-auto bg-black">
-                      <ModalComment
-                        username={user.username}
-                        profileImg={user.profileImage ? user.profileImage.path : ""}
-                        message={message}
-                        handleChange={handleMessageChange}
-                        handleSubmit={handleSubmitComment}
-                      />
-                    </div>
-                  ) : (
-                    <>
-                      <div className="overflow-scroll no-scrollbar">
-                        {comments.map((item, index) => (
-                          <Comment key={index} 
-                            username={item.creator.username} 
-                            comment={item.comment} 
-                            likes={item.likes.length} 
-                            timeStamp={item.date}
-                            handleCommentLike={handleCommentLike}
-                            commentId={item._id}
-                            isLiked={item.isLiked}
-                          >
-                            <CircleBackground md={true}>
-                              <UserIcon name={item.username} imgName={item.creator.profileImage ? item.creator.profileImage.path : ""}/>
-                            </CircleBackground>
-                          </Comment>
-                        ))}
-                      </div>
+              <div>
+                <FontAwesomeIcon className="text-2xl hover:cursor-pointer" icon={faComment} onClick={() => setShowModal(true)}/>
+              </div>
+              <Modal showModal={showModal}>
+                <ModalHeader
+                  title={comments.length} 
+                  handlePinLike={handlePinLike} 
+                  pinLiked={pinLiked}
+                  setShowModal={setShowModal}
+                  toggleDeleteModal={toggleDeleteModal}
+                  isCreator={isCreator}
+                  showDeleteModal={showDeleteModal}
+                >
+                  <div className="flex flex-col gap-4 w-full h-96">
+                  {
+                    comments.length < 1 ? (
                       <div className="mt-auto bg-black">
                         <ModalComment
                           username={user.username}
@@ -138,10 +150,39 @@ const PinEcho = ({
                           handleSubmit={handleSubmitComment}
                         />
                       </div>
-                    </>
-                  )
-                }
-                </div>
+                    ) : (
+                      <>
+                        <div className="overflow-scroll no-scrollbar">
+                          {comments.map((item, index) => (
+                            <Comment key={index} 
+                              username={item.creator.username} 
+                              comment={item.comment} 
+                              likes={item.likes.length} 
+                              timeStamp={item.date}
+                              handleCommentLike={handleCommentLike}
+                              commentId={item._id}
+                              isLiked={item.isLiked}
+                            >
+                              <CircleBackground md={true}>
+                                <UserIcon name={item.username} imgName={item.creator.profileImage ? item.creator.profileImage.path : ""}/>
+                              </CircleBackground>
+                            </Comment>
+                          ))}
+                        </div>
+                        <div className="mt-auto bg-black">
+                          <ModalComment
+                            username={user.username}
+                            profileImg={user.profileImage ? user.profileImage.path : ""}
+                            message={message}
+                            handleChange={handleMessageChange}
+                            handleSubmit={handleSubmitComment}
+                          />
+                        </div>
+                      </>
+                    )
+                  }
+                  </div>
+                </ModalHeader>
               </Modal>
             </div>
 
@@ -209,7 +250,7 @@ const PinEcho = ({
           </div>
         </div>
       </section>
-    </Layout>
+    </>
   )
 };
 
