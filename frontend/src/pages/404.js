6e04101b094761507e1b7ba6c58f3cb205856c5e
Steve <stevewu24@gmail.com>
2024-01-23 11:33:28 -0800

    added 404 page not found

---
+++ frontend/src/pages/404.js
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import { fetchCredentials } from '../utils/auth';
+
+export default function Custom404() {
+  useEffect(() => {
+    const isAuthenticated = fetchCredentials();
+    if (!isAuthenticated) {
+      router.push('/landing');
+    }
+  }, []);
+
+  return <h1>404 - Page Not Found</h1>
+}
