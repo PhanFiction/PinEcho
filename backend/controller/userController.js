45d7563877be2c9f402aa9a6f649ae4ff07d0335
Steve <stevewu24@gmail.com>
2024-01-23 11:23:50 -0800

    updated user controller to update user information

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const config = require('../config/config');
-const { uploadImg } = require('../utils/cloudinaryService');
+const { uploadImg, deleteImg } = require('../utils/cloudinaryService');
 const verifyToken = require('../utils/verifyToken');
 
 // returns all users from database
@@ -15,9 +15,8 @@ exports.getUser = async (req, res) => {
   try{
     const cookie = req.headers.cookie.split(';')[0].split("authToken=")[1];
     const decodedToken = verifyToken(cookie);
+    if(!decodedToken) return res.status(401).send({error: 'Not authorized'});
     const foundUser = await User.findById(decodedToken.id).select('-passwordHash -v');
-
-    if(!decodedToken) return res.send({error: 'Not authorized'});
     res.status(200).send(foundUser);
   }catch(error){
     res.status(400).send({error});
@@ -26,42 +25,53 @@ exports.getUser = async (req, res) => {
 
 // update user information
 exports.updateUserInfo = async (req, res) => {
-  const { username, email, name, password, profileImg } = req.body;
-
-  let imgResult = '';
-  const saltRounds = 10;
-  let passwordHash = null;
-  
-  if(password !== '' || password.length < 5) {
-    passwordHash = await bcrypt.hash(password, saltRounds);
-  };
-
-  const token = req.cookie['session_id'];
-  const decoded = jwt.verify(token, config.SECRET_KEY);
-  if(!decoded) return res.send({error: 'Not authorized'});
-
-  const foundUser = await User.findById({id: decoded.id});
-  if(!foundUser) return res.status(401).send({error: 'user not found'});
-
-  // delete profile image if user provides profile img
-  if(profileImg.length > 1) {
-    await deleteImg(foundUser.imgPath.publicId, 'profile');
-    imgResult = await uploadImg(profileImg, 'profile');
-  }
-
-  const updatedUserInfo = {
-    username,
-    name,
-    email,
-    passwordHash: passwordHash === null ? foundUser.passwordHash : passwordHash,
-    profileImage: {
-      path: result.url,
-      publicId: result.public_id,
-    },
-  };
-
   try {
-    await User.findByIdAndUpdate({id: decoded.id}, updatedUserInfo);
+    const { username, email, firstName, lastName, password, newPassword, profileImage } = req.body;
+    const passwordMin = 4;
+
+    let imgResult = '';
+    const saltRounds = 10;
+    let passwordHash = null;
+    
+    if(!req.headers.cookie) return res.send({error: 'Not authorized'});
+    
+    const cookie = req.headers.cookie.split(';')[0].split("authToken=")[1];
+    const decodedToken = verifyToken(cookie);
+    if(!decodedToken) return res.send({error: 'Not authorized'});
+
+    const foundUser = await User.findById(decodedToken.id);
+    if(!foundUser) return res.status(401).send({error: 'user not found'});
+
+    if(password.length > passwordMin && newPassword.length > passwordMin) {
+      const correctPassword = await bcrypt.compare(password, foundUser.passwordHash);
+
+      if(!correctPassword) return res.status(401).send("Unauthorized: Incorrect password");
+
+      if(password !== newPassword) passwordHash = await bcrypt.hash(newPassword, saltRounds);
+      else passwordHash = foundUser.passwordHash;
+    }else{
+      return res.status(401).send("Unauthorized: Incorrect password");
+    }
+
+    // delete profile image if it exist
+    if(profileImage && foundUser.imgPath) {
+      await deleteImg(foundUser.imgPath.publicId, 'profile');
+    }
+    imgResult = await uploadImg(profileImage, 'profile');
+
+    const updatedUserInfo = {
+      username,
+      firstName,
+      lastName,
+      email,
+      passwordHash: passwordHash,
+      profileImage: {
+        path: imgResult.url,
+        publicId: imgResult.public_id,
+      },
+    };
+
+    await User.findByIdAndUpdate(decodedToken.id, updatedUserInfo, {new:true});
     res.status(201).send({'success': 'user information updated'});
   }catch(error){
     res.status(401).send({'error': 'could not update user information'});
