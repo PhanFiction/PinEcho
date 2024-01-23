0b24ba6b2a7bc0f86c5e06d71a9737a1f3021382
Steve <stevewu24@gmail.com>
2024-01-23 11:24:39 -0800

    updated auth routes, removed user routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,8 +5,10 @@ const validateServices = require('../utils/validate');
 
 router.post('/login', validateServices.validateLoginCredentials, authController.login);
 router.post('/signup', validateServices.validateSignUpCredentials, authController.signUp);
+router.put('/user', userController.updateUserInfo);
 router.get('/user', userController.getUser);
 router.get('/users', userController.getUsers);
 router.get('/logout', authController.logout);
+router.get('/authentication', authController.checkAuthenticatiion);
 
 module.exports = router;
