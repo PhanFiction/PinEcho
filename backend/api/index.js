130c74c22605dad33db91ea1199150139a2aaf77
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-05-21 22:37:04 -0700

    Added comments

--- backend/api/index.js
+++ backend/api/index.js
@@ -15,16 +15,19 @@ const corsOptions = {
   credentials: true, // included credentials as true
 };
 
+// set core options
 app.use(cors(corsOptions));
 
-app.use(bodyParser.json({limit:'50mb'})); 
+app.use(bodyParser.json({limit:'50mb'})); // set limit to 50mb for body 
 app.use(bodyParser.urlencoded({extended:true, limit:'50mb'})); 
 
+// connect to database
 mongoose.connect(config.databaseURL)
   .then(() => console.log('Connected to database'));
   
-app.use(express.json());
+app.use(express.json()); // parse JSON bodies
 
+// routes
 app.use('/auth', authRoutes);
 app.use('/pins', pinRoutes);
 app.get('/', (req, res) => {
